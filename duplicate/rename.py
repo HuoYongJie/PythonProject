f62420c6e4439f199c27392c651261368c1cc3df
HuoYongJie <349574068@qq.com>
2018-01-12 10:31:24 +0800

    no message

--- duplicate/rename.py
+++ duplicate/rename.py
@@ -4,8 +4,8 @@ import os
 
 def rename():
      i=0
-     path="/Users/huoyongjie/Desktop/截图/收文图片/"
-     if os.path.isfile(path) == True:
+     path='/Users/huoyongjie/Desktop/girl11/'
+     if os.path.isdir(path) == True:
 
          filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
          for files in filelist:#遍历所有文件
@@ -15,7 +15,7 @@ def rename():
                     continue
             filename=os.path.splitext(files)[0]#文件名
             filetype=os.path.splitext(files)[1]#文件扩展名
-            Newdir=os.path.join(path,'收文流程-'+str(i)+filetype)#新的文件路径
+            Newdir=os.path.join(path,'插图-'+str(i+60)+filetype)#新的文件路径
             os.rename(Olddir,Newdir) #重命名
      else:
            print("没有该文件")
